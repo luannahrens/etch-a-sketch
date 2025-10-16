c956dd9bac909fac892b15039533bd81e4b72536
luannahrens <lahrens@gmail.com>
2025-10-15 21:38:50 -0700

    Added button to resize grip per user input

--- javascript.js
+++ javascript.js
@@ -1,9 +1,31 @@
 let container = document.getElementsByClassName('container')[0];
-for (i = 0; i < 256; i++){
-    let newDiv = document.createElement('div');
-    newDiv.textContent = i;
-    newDiv.className = "item";
-    container.appendChild(newDiv);
-    
+function drawGrid(size){
+    container.textContent = "";
+    for (i = 0; i < (size*size); i++){
+        let newDiv = document.createElement('div');
+        newDiv.textContent = i;
+        newDiv.className = "item";
+        newDiv.style.height = `calc(100% * (1/${size}))`;
+        newDiv.style.width = `calc(100% * (1/${size}))`;
+        newDiv.addEventListener('mouseenter', function() {
+            // Code to execute when the mouse enters the element
+            console.log('Mouse entered!');
+            newDiv.style.backgroundColor = 'indigo';
+        });
+        container.appendChild(newDiv);
+        
+    }
 }
+
+drawGrid(16);
+btnSize = document.getElementById("size");
+btnSize.addEventListener("click", () => {
+    let userInput = prompt("Please enter a new grid size: ");
+    if (userInput <= 100) {
+        drawGrid(userInput);
+    } else {
+        alert ("Size must be 100 or lower.");
+    }
+});
+
 
